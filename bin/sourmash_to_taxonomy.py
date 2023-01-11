2e2513dafa9e42165699d04f25abba1f9ec6c083
vikas peddu <vikas.peddu@gmail.com>
2023-01-11 13:55:00 -0800

    dlakfj

--- bin/sourmash_to_taxonomy.py
+++ bin/sourmash_to_taxonomy.py
@@ -15,8 +15,10 @@ with open(sys.argv[1], 'r') as file:
         # first row is header so skipping it here
         if row[6] and row[6] != 'genus':
             #print(row[6], ncbi.get_name_translator[str(row[6])])
-            tid = ncbi.get_name_translator([str(row[6])])
-            genus_taxids.append(tid[row[6]][0])
+            # not all taxa are up to date in the NCBI sqlite database. Small number but needs to be fixed 
+            if tid: 
+                tid = ncbi.get_name_translator([str(row[6])])
+                genus_taxids.append(tid[row[6]][0])
 
 df = pd.DataFrame(genus_taxids) 
 filename = sys.argv[2] + '.sourmash_to_genus.txt'
